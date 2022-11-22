2625624ec837ef7e592c88c52dc99ad69dae1714
vanquiche <stevevang2010@hotmail.com>
2022-11-22 22:01:27 +0900

    update tests

--- client/src/components/__test__/ValidationStatus.test.tsx
+++ client/src/components/__test__/ValidationStatus.test.tsx
@@ -21,25 +21,27 @@ const validationValidated: TouchObject = {
 };
 
 test('received prop is null', () => {
-  render(<ValidationStatus validation={null} />);
+  render(<ValidationStatus validation={null} forInput='test' />);
   const wrapper = screen.getByTestId('validation-wrapper');
   expect(wrapper.hasChildNodes()).toBe(false);
 });
 
 test('is loading', () => {
-  render(<ValidationStatus validation={validationLoading} />);
-  const loadingIcon = screen.getByTestId('validation-loading');
+  render(<ValidationStatus validation={validationLoading} forInput='test' />);
+  const loadingIcon = screen.getByTestId('validation-test-loading');
   expect(loadingIcon).toBeInTheDocument();
 });
 
 test('is invalidated', () => {
-  render(<ValidationStatus validation={validationInvalidated} />);
-  const invalidatedIcon = screen.getByTestId('validation-invalidated');
+  render(
+    <ValidationStatus validation={validationInvalidated} forInput='test' />
+  );
+  const invalidatedIcon = screen.getByTestId('validation-test-invalid');
   expect(invalidatedIcon).toBeInTheDocument();
 });
 
 test('is validated', () => {
-  render(<ValidationStatus validation={validationValidated} />);
-  const validatedIcon = screen.getByTestId('validation-validated');
+  render(<ValidationStatus validation={validationValidated} forInput='test' />);
+  const validatedIcon = screen.getByTestId('validation-test-valid');
   expect(validatedIcon).toBeInTheDocument();
 });
